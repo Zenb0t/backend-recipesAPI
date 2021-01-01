4983f840b73bc9e675ad1363d8a24b945de8717d
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2020-12-31 22:01:09 -0800

    updated local dependencies

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dbName = "test";
 
-const uri = `mongodb+srv://dbmexepanela:bananadademarmelo@recipe-cluster1.fhuul.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const uri = process.env.URI_MONGODB ||`mongodb+srv://dbmexepanela:bananadademarmelo@recipe-cluster1.fhuul.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 
 mongoose.connect(uri, {
